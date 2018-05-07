9a1c145225631ead8d236edf39c0da42b3f6346d
vandershalan <t.szalankiewicz@gmail.com>
2018-05-07 19:53:51 +0200

    First  commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,46 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { ErrorHandler, NgModule } from '@angular/core';
+import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { StatusBar } from '@ionic-native/status-bar';
+
+import { MyApp } from './app.component';
+import { HomePage } from '../pages/home/home';
+
+// Import the AF2 Module
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+
+// AF2 Settings
+export const firebaseConfig = {
+    apiKey: "AIzaSyAbgLEXCtwDTIrAMeYP54ovupW7eI_rvu8",
+    authDomain: "homorg-hom-list.firebaseapp.com",
+    databaseURL: "https://homorg-hom-list.firebaseio.com",
+    projectId: "homorg-hom-list",
+    storageBucket: "homorg-hom-list.appspot.com",
+    messagingSenderId: "797168590106"
+};
+
+@NgModule({
+  declarations: [
+    MyApp,
+    HomePage
+  ],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    HomePage
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    {provide: ErrorHandler, useClass: IonicErrorHandler}
+  ]
+})
+export class AppModule {}
---
+++ src/pages/home/home.ts
@@ -0,0 +1,118 @@
+import { Component } from '@angular/core';
+import { NavController, AlertController, ActionSheetController } from 'ionic-angular';
+import { AngularFireDatabase, AngularFireList } from 'angularfire2/database';
+import { Observable } from "rxjs/Observable";
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+    listsList: AngularFireList<any>;
+    lists: Observable<any[]>;
+    
+    constructor(public navCtrl: NavController, public alertCtrl: AlertController, public afDatabase: AngularFireDatabase, public actionSheetCtrl: ActionSheetController) {
+        this.listsList = afDatabase.list('/lists');
+        this.lists = this.listsList.valueChanges();
+    }
+    
+    
+    addList(){
+        let prompt = this.alertCtrl.create({
+            title: 'List Name',
+            message: "Enter a name for this new list",
+            inputs: [
+              {
+                name: 'title',
+                placeholder: 'Title'
+              },
+            ],
+            buttons: [
+              {
+                text: 'Cancel',
+                handler: data => {
+                  console.log('Cancel clicked');
+                }
+              },
+              {
+                text: 'Save',
+                handler: data => {
+                  const newListRef = this.listsList.push({});
+         
+                  newListRef.set({
+                    id: newListRef.key,
+                    title: data.title
+                  });
+                }
+              }
+            ]
+          });
+          prompt.present();
+    }
+
+
+    showOptions(listId, listTitle) {
+      let actionSheet = this.actionSheetCtrl.create({
+        title: 'What do you want to do with: ' + listTitle + '?',
+        buttons: [
+          {
+            text: 'Delete List',
+            role: 'destructive',
+            handler: () => {
+              this.removeList(listId);
+            }
+          },{
+            text: 'Update title',
+            handler: () => {
+              this.updateList(listId, listTitle);
+            }
+          },{
+            text: 'Cancel',
+            role: 'cancel',
+            handler: () => {
+              console.log('Cancel clicked');
+            }
+          }
+        ]
+      });
+      actionSheet.present();
+    }
+
+
+    removeList(listId: string){
+        this.listsList.remove(listId);
+    }
+
+    
+    updateList(listId, listTitle){
+      let prompt = this.alertCtrl.create({
+        title: 'List Name',
+        message: "Update the name for this list",
+        inputs: [
+          {
+            name: 'title',
+            placeholder: 'Title',
+            value: listTitle
+          },
+        ],
+        buttons: [
+          {
+            text: 'Cancel',
+            handler: data => {
+              console.log('Cancel clicked');
+            }
+          },
+          {
+            text: 'Save',
+            handler: data => {
+              this.listsList.update(listId, {
+                title: data.title
+              });
+            }
+          }
+        ]
+      });
+      prompt.present();
+    }
+}
